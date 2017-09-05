fc3442ce71d2f7b2600e29ec1d04a42ec60660b5
cancan <1048042379@qq.com>
2017-09-06 00:18:47 +0800

    make check of pwd

--- app/static/js/index_base.js
+++ app/static/js/index_base.js
@@ -19,3 +19,25 @@ $('.my-navbar-dropdown').hover(
         $('#my-navbar-dropdown').slideUp();
     }
 )
+
+function checkPwd(p1, p2) {
+    if (p1 == p2) {
+        return true;
+    }
+};
+
+$('#btn-register').click(function () {
+    $p1 = $('#passwordR1');
+    $p2 = $('#passwordR2');
+    if (checkPwd($p1.val(), $p2.val())) {
+        $p1.parent().addClass('has-success has-feedback');
+        $p1.next().addClass('glyphicon glyphicon-ok form-control-feedback');
+        $p1.nextUntil().show();
+    }
+});
+
+$('#btn-register-cancel').click(function () {
+    $p1.parentsUntil().first().removeClass().addClass('col-md-10');
+    $p1.next().removeClass();
+    $p1.nextUntil().hide();
+});

f94df1f8b9828179a8ce185bbc1d051ba0283397
cancan <1048042379@qq.com>
2017-09-19 00:27:14 +0800

    add role

--- app/models.py
+++ app/models.py
@@ -88,3 +88,20 @@ def load_user(user_id):
     print 'current_user: ' + user_id
     user = User.query.get(int(user_id))
     return user
+
+
+class Permission:
+    """权限常量"""
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')

d580b4f873c72e27b4172a940cff21768e7d6639
goodcan <1048042379@qq.com>
2017-09-20 13:22:05 +0800

    repair bug of no attribute self.role

--- app/models.py
+++ app/models.py
@@ -19,8 +19,6 @@ class Role(db.Model):
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
 
-    # users = db.relationship('User', backref='role', lazy='dynamic')
-
     @staticmethod
     def insert_roles():
         roles = {
@@ -60,18 +58,19 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     confirmed = db.Column(db.Boolean, default=False)
-
+    role = None
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     users = db.relationship('Role', backref='users')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
-        if self.email == current_app.config['FLASK_ADMIN']:
-            self.role = Role.query.filter_by(permissions=0xff).first()
-            self.role_id = self.role.id
-        else:
-            self.role = Role.query.filter_by(default=True).first()
-            self.role_id = self.role.id
+        if self.role is None:
+            if self.email == current_app.config['FLASK_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+                self.role_id = self.role.id
+            else:
+                self.role = Role.query.filter_by(default=True).first()
+                self.role_id = self.role.id
 
     def can(self, permissions):
         return self.role is not None and \

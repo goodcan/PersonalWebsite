0d49fc1db06c5b30fccf79f9d6f7444cc803ec6c
goodcan <1048042379@qq.com>
2017-09-27 10:14:55 +0800

    add try for operation.py

--- app/auth/operation.py
+++ app/auth/operation.py
@@ -64,9 +64,12 @@ def login():
             next = request.args.get('next')
             if next:
                 url_list = next.split('/')
-                if url_list[2] == 'user_profile':
-                    url_list[3] = username
-                    next = '/'.join(url_list)
+                try:
+                    if url_list[2] == 'user_profile':
+                        url_list[3] = username
+                        next = '/'.join(url_list)
+                except:
+                    print next
 
                 print next
 

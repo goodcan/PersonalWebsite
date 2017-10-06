ea8b97cde4244c3359d2484d0541f7ec9d291e31
cancan <1048042379@qq.com>
2017-10-06 20:28:56 +0800

    finish show_create_time

--- app/common.py
+++ app/common.py
@@ -14,13 +14,17 @@ def response_messages(response, title, content):
 
 def deal_time(start_time):
     end_time = datetime.now()
-    D_value = end_time - start_time
+    if end_time >= start_time:
+        D_value = end_time - start_time
+    else:
+        D_value = end_time - end_time
     if D_value.days > 0:
         return str(D_value.days) + u'天前'
     else:
         s = D_value.seconds
         h = s / 3600
         m = s % 3600 / 60
+        # print s, h
         if h > 0:
             return str(h) + u'小时前'
         elif m > 0:

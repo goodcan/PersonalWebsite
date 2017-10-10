c60f7ac3035583acc5716e030bbfdbd31ec1b6a0
goodcan <1048042379@qq.com>
2017-10-10 17:13:31 +0800

    add btn_refresh

--- app/static/js/auth/index.js
+++ app/static/js/auth/index.js
@@ -33,6 +33,33 @@ $('#btn-search').click(function () {
 
     });
 });
+
+$('#btn-refresh').click(function () {
+    $('#search-content').val('')
+    search_data = {
+        'page': '1',
+        'project_name': $('#project-search-select').val(),
+        'class_name': $('#class-search-select').val(),
+        'search_content': ''
+    };
+    $.get('/auth/index/search/', search_data, function (data) {
+        $('#all-articles').html('');
+        if (data['status']) {
+            load_data = data['data']['load_data'];
+            l = load_data.length;
+            for (i = 0; i < l; i++) {
+                load_all_content_append($('#all-articles'), load_data[i]);
+            }
+            $('#all-articles').append(load_base_div);
+        }
+        else {
+            $('#all-articles').css({'text-align': 'center'})
+                .html('<h4>' + data['data']['message'] + '</h4>');
+        }
+
+    });
+});
+
 $(window).scroll(function () {
     load_page_content(search_data);
 });
